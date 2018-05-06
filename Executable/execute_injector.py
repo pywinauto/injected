6645a62c9fd91e18b41e2dce1571342f34f215e1
drinkertea <spore17@mail.ru>
2018-05-06 13:58:03 +0300

    This commit broke all, but added for compare
    Actual code version in anoher pr

--- Executable/execute_injector.py
+++ Executable/execute_injector.py
@@ -1,28 +1,28 @@
 import sys
-from injector import inject_dll_to_process
-from injector import remote_call_void_func
-from pywinauto import Desktop
+import win32con
+from injector import *
+from ctypes import wintypes
+from mywin32enum import WIN_ID_TO_KEY
 
-# This code should be incapsulate to some module or class in library
-
-# NOTE: mb call thats script as subprocess with 64 or 32 bit python
-# MSG structure also has different sizes depends of pyhon type
+def print_winmsg(msg):
+    print "hWnd:" +  str(msg.hWnd)
+    print "message:" + (WIN_ID_TO_KEY[msg.message] if msg.message in WIN_ID_TO_KEY else str(msg.message))
+    print "wParam:" +  str(msg.wParam)
+    print "lParam:" +  str(msg.lParam)
+    print "time:" +  str(msg.time)
+    print "pt:" + str(msg.pt.x) + ',' + str(msg.pt.x)
 
 if (len(sys.argv) != 3):
     print("Usage: {} <PID> <Path To DLL>".format(sys.argv[0]))
     print("Eg: {} 1111 C:\\test\messagebox.dll".format(sys.argv[0]))
     sys.exit(0)
 
-try:
-    dll_path = sys.argv[2]
+sock = execute_workflow(sys.argv[1], sys.argv[2])
 
-    app = Desktop(backend="win32")[sys.argv[1]]
-
-    inject_dll_to_process(app, dll_path)
-    remote_call_void_func(app, dll_path, "SetMsgHook")
-    # will be used in data listener or rewriten
-    # sock_port = ctypes.cdll.LoadLibrary(dll_path).getSocketPort()
-except (RuntimeError, AttributeError) as exc:
-    print(exc)
-except IndexError:
-    print("Invalid process name.")
+while(1):
+    msg = wintypes.MSG()
+    m = sock.recvfrom(1024)
+    ctypes.memmove(ctypes.pointer(msg),m[0],ctypes.sizeof(msg))
+    print_winmsg(msg)
+    if msg.message == win32con.WM_QUIT:
+        break;
--- Executable/injector.py
+++ Executable/injector.py
@@ -1,9 +1,27 @@
 from pywinauto.handleprops import processid
-
+from pywinauto.handleprops import is64bitprocess
+from pywinauto.timings import TimeoutError
+from pywinauto import Desktop
+from socket import *
+import win32event
 import cfuncs
+import ctypes
+import sys
+import os
+
+#TODO: incapsulate to class
 
 def get_process_handle(pid):
     return cfuncs.OpenProcess(cfuncs.PROCESS_ALL_ACCESS, False, pid)
+
+def create_remote_thread_with_timeout(h_process, proc_address, arg_address, timeout_ms = 1000, call_err_text = "", timeout_err_text = ""):
+    thread_handle = cfuncs.CreateRemoteThread(h_process, None, 0, proc_address, arg_address, 0, None)
+    if not thread_handle:
+        raise RuntimeError(call_err_text)
+    # TODO: add timeout to pywinauto timings and replace
+    ret = cfuncs.WaitForSingleObject(thread_handle, ctypes.wintypes.DWORD(timeout_ms))
+    if ret == cfuncs.WAIT_TIMEOUT:
+        raise TimeoutError(timeout_err_text)
 
 def inject_dll_to_process(app, dll_path):
     # Get dll path length
@@ -19,18 +37,57 @@ def inject_dll_to_process(app, dll_path):
         raise AttributeError("Couldn't write data to process memory, check python acceess.")
 
     # Resolve LoadLibraryA Address
-    h_kernel32 = cfuncs.GetModuleHandle("kernel32.dll")
+    h_kernel32 = cfuncs.GetModuleHandleA("kernel32.dll")
     h_loadlib = cfuncs.GetProcAddress(h_kernel32, "LoadLibraryA")
 
     # Now we createRemoteThread with entrypoiny set to LoadLibraryA and pointer to DLL path as param
-    if not cfuncs.CreateRemoteThread(h_process, None, 0, h_loadlib, arg_address, 0, None):
-        raise RuntimeError("Couldn't create remote thread, application and python version must have same version (x32 or x64)")
+    create_remote_thread_with_timeout(h_process, h_loadlib, arg_address, 1000,
+        "Couldn't create remote thread, application and python version must have same version (x32 or x64)",
+        "Inject time out")
+
+def get_dll_proc_address(dll_path, proc_name):
+    lib = cfuncs.LoadLibraryA(dll_path)
+    return cfuncs.GetProcAddress(lib, proc_name)
 
 def remote_call_void_func(app, dll_path, func_name):
     h_process = get_process_handle(processid(app.handle))
-    lib = cfuncs.LoadLibraryA(dll_path)
-
-    proc_address = cfuncs.GetProcAddress(lib, func_name)
+    proc_address = get_dll_proc_address(dll_path, func_name)
 
     if not cfuncs.CreateRemoteThread(h_process, None, 0, proc_address, 0, 0, None):
-        raise RuntimeError("Couldn't create remote thread, application and python version must have same version (x32 or x64)")
+        raise RuntimeError("Couldn't create remote thread, dll not injected, inject and try again!")
+
+def remote_call_int_param_func(app, dll_path, func_name, param):
+    import ctypes
+    pid = processid(app.handle)
+    h_process = get_process_handle(pid)
+
+    # resolve paramtype for different applications
+    a = ctypes.c_int64(param) if is64bitprocess(pid) else ctypes.c_int32(param)
+
+    arg_address = cfuncs.VirtualAllocEx(h_process, 0, ctypes.sizeof(a), cfuncs.VIRTUAL_MEM, cfuncs.PAGE_READWRITE)
+    if not cfuncs.WriteProcessMemory(h_process, arg_address, ctypes.byref(a), ctypes.sizeof(a), 0):
+        raise AttributeError("Couldn't write data to process memory, check python acceess.")
+
+    proc_address = get_dll_proc_address(dll_path, func_name)
+
+    create_remote_thread_with_timeout(h_process, proc_address, arg_address, 1000,
+        "Couldn't create remote thread, dll not injected, inject and try again!",
+        "{0}(int) finction call time out".format(func_name))
+
+# inected pywinauto's dll to the process and returns socket for listhen info
+def execute_workflow(process_name, dll_path):
+    # resolve arguments
+    dll_path = os.path.abspath(dll_path)
+    app = Desktop(backend="win32")[process_name]
+
+    # Inject dll, TODO: choose some particular dll (32 or 64, unicode or ANSI)
+    inject_dll_to_process(app, dll_path)
+
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.bind(('',0))
+    port = s.getsockname()[1]
+
+    remote_call_int_param_func(app, dll_path, "InitSocket", port)
+    remote_call_void_func(app, dll_path, "SetMsgHook")
+
+    return s
