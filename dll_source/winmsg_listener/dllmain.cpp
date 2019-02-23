a06288358b044197559806e168741d28ca766f9b
drinkertea <spore17@mail.ru>
2019-02-23 14:04:45 +0300

    Update dllmain.cpp

--- dll_source/winmsg_listener/dllmain.cpp
+++ dll_source/winmsg_listener/dllmain.cpp
@@ -8,51 +8,253 @@
 #include <string>
 #include <sstream>
 #include <thread>
+#include <vector>
 #include <mutex>   
 #include <condition_variable>
 #include "TlHelp32.h"
 #pragma comment (lib, "Ws2_32.lib")
-#pragma warning(disable : 4996)
 
-HHOOK g_hHook = NULL;
-HINSTANCE g_hDll = NULL;
+HHOOK g_hHook = nullptr;
+HINSTANCE g_hDll = nullptr;
 
 BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved);
-extern "C" __declspec(dllexport) BOOL SetMsgHook(char* path);    // Exported
+extern "C" __declspec(dllexport) BOOL SetMsgHook(wchar_t* path);    // Exported
 extern "C" __declspec(dllexport) BOOL UnsetMsgHook();
-extern "C" __declspec(dllexport) BOOL OpenLogFile(char* path);
+extern "C" __declspec(dllexport) BOOL OpenLogFile(wchar_t* path);
 extern "C" __declspec(dllexport) BOOL InitSocket(int* port);
 LRESULT CALLBACK SysMsgProc(int nCode, WPARAM wParam, LPARAM lParam);
 
-SOCKET sock;
-char buff[sizeof(MSG)];
-struct sockaddr_in Sender_addr;
-bool socketInitialized = false;
-std::ofstream myfile;
+class InjectorManager {
+private:
+	//
+
+	void print_log(const std::wstring& message)
+	{
+		if (m_log_file.is_open()) {
+			m_log_file << message << std::endl;
+		}
+	}
+
+	void message_to_file(LPMSG aMsg) {
+		if (m_log_file.is_open()) {
+			std::wstringstream ss;
+			ss << aMsg->time << ";";
+			ss << aMsg->message << ";";
+			ss << aMsg->hwnd << ";";
+			ss << aMsg->lParam << ";";
+			ss << aMsg->wParam << ";";
+			ss << aMsg->pt.x << ";";
+			ss << aMsg->pt.y << ";";
+			print_log(ss.str());
+		}
+	}
+
+	void cpu_save_infinite_wait() {
+		
+		std::unique_lock<std::mutex> lock(m_hook_mutex);
+		while (!m_hook_stop_thread)
+		{
+			m_hook_cv.wait(lock);
+		}
+
+		std::vector<char> message_buffer(m_msg_size, 0);
+		//initialize_socket(m_port);
+		print_log(L"TRYYYYYYYYYYYYYY!");
+		if (m_socket_initialized && sendto(m_socket, message_buffer.data(), (int)m_msg_size, 0, (sockaddr*)&m_sender_address, sizeof(m_sender_address)) < 0)
+		{
+			wchar_t *s = NULL;
+			FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+				NULL, WSAGetLastError(),
+				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+				(LPWSTR)&s, 0, NULL);
+			std::wstring skdaksjk(s);
+			LocalFree(s);
+			print_log(skdaksjk);
+			closesocket(m_socket);
+			m_socket_initialized = false;
+			print_log(L"FAIIIIIIIIIIL!");;
+		}
+
+	}
+
+	std::wstring get_last_error()
+	{
+		HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
+		_com_error err(hr);
+		LPCTSTR errMsg = err.ErrorMessage();
+		return std::wstring();
+	}
+
+	SOCKET m_socket;
+	struct sockaddr_in m_sender_address;
+	bool m_socket_initialized = false;
+	std::wofstream m_log_file;
+	const static size_t m_msg_size = sizeof(MSG);
+	bool							m_hook_stop_thread = false;
+	std::unique_ptr<std::thread>	m_hook_thread;
+	std::condition_variable			m_hook_cv;
+	std::mutex						m_hook_mutex;
+	bool							m_socket_stop_thread = false;
+	std::unique_ptr<std::thread>	m_socket_thread;
+	std::condition_variable			m_socket_cv;
+	std::mutex						m_socket_mutex;
+	bool m_wait;
+	int m_port;
+public:
+	InjectorManager() {}
+	InjectorManager(const InjectorManager&) = delete;
+	InjectorManager& operator=(const InjectorManager &) = delete;
+	InjectorManager(InjectorManager &&) = delete;
+	InjectorManager & operator=(InjectorManager &&) = delete;
+
+
+	BOOL open_log_file(wchar_t* path) {
+		if (path)
+			m_log_file.open(path);
+		return m_log_file.is_open();
+	}
+
+	BOOL initialize_socket(int port) {
+		m_port = port;
+		//
+		//m_socket_thread = std::make_unique<std::thread>([&] {
+			WSADATA wsaData = {};
+			WSAStartup(MAKEWORD(2, 2), &wsaData);
+			m_socket = socket(AF_INET, SOCK_DGRAM, 0);
+			if (m_socket == INVALID_SOCKET) {
+				return FALSE;
+			}
+			BOOL enabled = TRUE;
+			if (setsockopt(m_socket, SOL_SOCKET, SO_BROADCAST, (char*)&enabled, sizeof(BOOL)) < 0) {
+				return FALSE;
+			}
+			m_sender_address.sin_family = AF_INET;
+			m_sender_address.sin_port = htons(m_port);
+			m_sender_address.sin_addr.s_addr = inet_addr("localhost");
+			m_socket_initialized = true;
+
+			{
+				std::unique_lock<std::mutex> lock(m_socket_mutex);
+				while (!m_socket_stop_thread)
+				{
+					m_socket_cv.wait(lock);
+				}
+			}
+
+
+
+		//});
+		return TRUE;
+	}
+
+	void set_msg_hook(wchar_t* path = nullptr)
+	{
+		MessageBoxA(0, "Attach now", "InitSocketCall", 0);
+		//m_hook_thread = std::make_unique<std::thread>([&] {
+			OpenLogFile(path);
+			g_hHook = SetWindowsHookEx(WH_GETMESSAGE, SysMsgProc, g_hDll, 0);
+			if (g_hHook != nullptr)
+			{
+				print_log(L"Hook successfully set!");
+				print_log(L"time;hwnd;lParam;wParam;message;pt.x;pt.y");
+			}
+			else
+			{
+				print_log(L"Error when hook set: " + get_last_error());
+				//return FALSE;
+			}
+			cpu_save_infinite_wait();
+
+
+			std::vector<char> message_buffer(m_msg_size, 0);
+			//initialize_socket(m_port);
+			print_log(L"TRYYYYYYYYYYYYYY!");
+			if (m_socket_initialized && sendto(m_socket, message_buffer.data(), (int)m_msg_size, 0, (sockaddr*)&m_sender_address, sizeof(m_sender_address)) < 0)
+			{
+				wchar_t *s = NULL;
+				FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+					NULL, WSAGetLastError(),
+					MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+					(LPWSTR)&s, 0, NULL);
+				std::wstring skdaksjk(s);
+				LocalFree(s);
+				print_log(skdaksjk);
+				closesocket(m_socket);
+				m_socket_initialized = false;
+				print_log(L"FAIIIIIIIIIIL!");;
+			}
+
+		//});
+	}
+
+	void send_msg(const MSG* msg)
+	{
+		std::vector<char> message_buffer(m_msg_size, 0);
+		//message_to_file(*msg);
+		memcpy(message_buffer.data(), msg, m_msg_size);
+		if (m_socket_initialized && sendto(m_socket, message_buffer.data(), (int)m_msg_size, 0, (sockaddr*)&m_sender_address, sizeof(m_sender_address)) < 0)
+		{
+			closesocket(m_socket);
+			m_socket_initialized = false;
+			print_log(L"Socket closed!");
+		}
+	}
+
+	void unload() {
+
+		std::vector<char> message_buffer(m_msg_size, 0);
+		//initialize_socket(m_port);
+		print_log(L"TRYYYYYYYYYYYYYY!");
+		if (m_socket_initialized && sendto(m_socket, message_buffer.data(), (int)m_msg_size, 0, (sockaddr*)&m_sender_address, sizeof(m_sender_address)) < 0)
+		{
+			wchar_t *s = NULL;
+			FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+				NULL, WSAGetLastError(),
+				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+				(LPWSTR)&s, 0, NULL);
+			std::wstring skdaksjk(s);
+			LocalFree(s);
+			print_log(skdaksjk);
+			closesocket(m_socket);
+			m_socket_initialized = false;
+			print_log(L"FAIIIIIIIIIIL!");;
+		}
+
+		{
+			std::unique_lock<std::mutex> lock(m_hook_mutex);
+			m_hook_stop_thread = true;
+			m_hook_cv.notify_one();
+
+		}
+		{
+			std::unique_lock<std::mutex> lock(m_socket_mutex);
+			m_socket_stop_thread = true;
+			m_socket_cv.notify_one();
+		}
+
+		if (g_hHook == nullptr)
+			return;
+
+		BOOL isUnhooked = UnhookWindowsHookEx(g_hHook);
+		if (isUnhooked)
+		{
+			print_log(L"Unhook");
+			g_hHook = nullptr;
+		}
+
+		if (m_log_file.is_open())
+			m_log_file.close();
+		m_wait = false;
+	}
+
+	static auto& instance() {
+		static InjectorManager test;
+		return test;
+	}
+};
 
 extern "C" __declspec(dllexport) HINSTANCE getDllHinstance() {
     return g_hDll;
-}
-
-void printLog(const std::string& message)
-{
-    if (myfile.is_open()) {
-        myfile << message << std::endl;
-    }
-}
-
-void messageToFile(LPMSG aMsg) {
-    if (myfile.is_open()) {
-        std::stringstream ss;
-        ss << aMsg->time << ";";
-        ss << aMsg->message << ";";
-        ss << aMsg->hwnd << ";";
-        ss << aMsg->lParam << ";";
-        ss << aMsg->wParam << ";";
-        ss << aMsg->pt.x << ";";
-        ss << aMsg->pt.y << ";";
-        printLog(ss.str());
-    }
 }
 
 BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
@@ -63,88 +265,26 @@ BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
         g_hDll = hinstDLL;
         return true;
     case DLL_PROCESS_DETACH:
-        UnsetMsgHook();
+		InjectorManager::instance().unload();
         return true;
     }
 }
 
-BOOL OpenLogFile(char* path) {
-    if (path)
-        myfile.open(path);
-    return myfile.is_open();
-}
-
-void infiniteWait() {
-    std::condition_variable cv;
-    std::mutex m;
-    std::unique_lock<std::mutex> lock(m);
-    cv.wait(lock, [] {return false; });
+BOOL OpenLogFile(wchar_t* path) {
+    return InjectorManager::instance().open_log_file(path);
 }
 
 BOOL InitSocket(int* port) {
-    //MessageBoxA(0, "Attach now", "InitSocketCall", 0);
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock == INVALID_SOCKET) {
-        return FALSE;
-    }
-    BOOL enabled = TRUE;
-    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)&enabled, sizeof(BOOL)) < 0) {
-        return FALSE;
-    }
-    Sender_addr.sin_family = AF_INET;
-    Sender_addr.sin_port = htons(*port);
-    Sender_addr.sin_addr.s_addr = inet_addr("localhost");
-    socketInitialized = true;
+	int _port = *port;
+	return InjectorManager::instance().initialize_socket(_port);
+}
+
+BOOL SetMsgHook(wchar_t* path = nullptr)
+{
+	InjectorManager::instance().set_msg_hook(path);
     return TRUE;
 }
 
-/*
-    Call this function after dll injected
-*/
-BOOL SetMsgHook(char* path = nullptr)
-{
-    //MessageBoxA(0, "Attach now", "SetMsgHookCall", 0);
-    OpenLogFile(path);
-    g_hHook = SetWindowsHookEx(WH_GETMESSAGE, SysMsgProc, g_hDll, 0);
-    if (g_hHook != NULL)
-    {
-        printLog("Hook successfully set!");
-        printLog("time;hwnd;lParam;wParam;message;pt.x;pt.y");
-    }
-    else
-    {
-        HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
-        _com_error err(hr);
-        LPCTSTR errMsg = err.ErrorMessage();
-        printLog("Error when hook set");
-        return FALSE;
-    }
-
-    infiniteWait();
-    return g_hHook != NULL;
-}
-
-BOOL UnsetMsgHook()
-{
-    if (g_hHook == NULL)
-        return FALSE;
-
-    BOOL isUnhooked = UnhookWindowsHookEx(g_hHook);
-    if (isUnhooked)
-    {
-        printLog("Unhook");
-        g_hHook = NULL;
-    }
-
-    if (myfile.is_open())
-        myfile.close();
-
-    closesocket(sock);
-
-    return isUnhooked;
-}
 
 LRESULT CALLBACK SysMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -153,16 +293,7 @@ LRESULT CALLBACK SysMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
 
     LPMSG aMsg = (LPMSG)lParam;
     if (aMsg) {
-        messageToFile(aMsg);
-        memcpy(&buff, aMsg, sizeof(*aMsg));
-        if (socketInitialized) {
-            if (sendto(sock, buff, sizeof(*aMsg), 0, (sockaddr *)&Sender_addr, sizeof(Sender_addr)) < 0)
-            {
-                closesocket(sock);
-                printLog("Socket closed!");
-                return CallNextHookEx(g_hHook, nCode, wParam, lParam);
-            }
-        }
+		InjectorManager::instance().send_msg((MSG*)aMsg);
     }
     return CallNextHookEx(g_hHook, nCode, wParam, lParam);
 }
