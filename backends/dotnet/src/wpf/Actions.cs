021fa2adf8ad20a17ec3ae841a1dc4b806e7a5b0
eltio <i.n.1998@inbox.ru>
2022-04-12 13:44:54 +0300

    wpf: move wpf backend source code to this repository

---
+++ backends/dotnet/src/wpf/Actions.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+
+namespace InjectedWorker
+{
+    internal abstract class ActionBase
+    {
+        public abstract string Name { get; }
+
+        public abstract Reply Run<T>(ControlsStorage<T> controls, IDictionary<string, dynamic> args) 
+            where T: class;
+
+
+        protected void CheckParamExists(IDictionary<string, dynamic> args, string name)
+        {
+            if (!args.ContainsKey(name))
+            {
+                throw new ErrorReplyException(ErrorCodes.MISSING_PARAM, "missing param: " + name);
+            }
+        }
+        protected void CheckValidControlId<T>(dynamic id, ControlsStorage<T> controls)
+            where T: class
+        {
+            if (id < 0)
+                throw new ErrorReplyException(ErrorCodes.INVALID_VALUE, "invalid element id (should be >= 0)");
+            if (controls != null)
+            {
+                if (id < 1)
+                    throw new ErrorReplyException(ErrorCodes.INVALID_VALUE, "invalid element id (should be >= 1)");
+
+                if (controls.GetControl(id) == null)
+                    throw new ErrorReplyException(ErrorCodes.NOT_FOUND, "element not found with ID: " + id);
+            }
+
+        }
+    }
+
+    internal abstract class GetChildrenAction<T> : ActionBase
+    {
+        public override string Name
+        {
+            get { return "GetChildren"; }
+        }
+    }
+
+    internal abstract class GetParent<T> : ActionBase
+    {
+        public override string Name
+        {
+            get { return "GetParent"; }
+        }
+    }
+
+    internal abstract class GetRectangle<T> : ActionBase
+    {
+        public override string Name
+        {
+            get { return "GetRectangle"; }
+        }
+    }
+
+    internal abstract class GetProperty<T> : ActionBase
+    {
+        public override string Name
+        {
+            get { return "GetProperty"; }
+        }
+    }
+
+    internal abstract class GetHandle<T> : ActionBase
+    {
+        public override string Name
+        {
+            get { return "GetHandle"; }
+        }
+    }
+
+    internal class GetTypeName : ActionBase
+    {
+        public override string Name
+        {
+            get { return "GetTypeName"; }
+        }
+
+        public override Reply Run<T>(ControlsStorage<T> controls, IDictionary<string, dynamic> args)
+        {
+            CheckParamExists(args, "element_id");
+            CheckValidControlId<T>(args["element_id"], controls);
+
+            object c = controls.GetControl(args["element_id"]);
+            if (c == null)
+                throw new ErrorReplyException(ErrorCodes.NOT_FOUND, "element not found: " + args["element_id"]);
+            else
+            {
+                DynamicValueReply reply = new DynamicValueReply(c.GetType().ToString());
+                return reply;
+            }
+        }
+    }
+
+}
+
+
---
+++ backends/dotnet/src/wpf/Replies.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace InjectedWorker
+{
+    public class Reply
+    {
+        public Reply(ErrorCodes statusCode)
+        {
+            Fields["status_code"] = (long)statusCode;
+        }
+
+        // TODO incapsulation
+        public Dictionary<string, dynamic> Fields = new Dictionary<string, dynamic>();
+    }
+
+    class ElementsReply : Reply
+    {
+        public ElementsReply(IEnumerable<long> elements)
+            : base(ErrorCodes.OK)
+        {
+            Fields["elements"] = new List<long>(elements);
+        }
+    }
+
+    class DynamicValueReply : Reply
+    {
+        public DynamicValueReply(dynamic value)
+            : base(ErrorCodes.OK)
+        {
+            if (value == null || value.GetType().IsPrimitive || value.GetType() == typeof(string))
+                Fields["value"] = value;
+            else
+                Serialize(value);
+        }
+
+        protected void Serialize(object o)
+        {
+            if (o == null)
+            {
+                Fields["value"] = "";
+            }
+            Fields["value"] = o.ToString();
+        }
+        protected void Serialize(Rect rect)
+        {
+            Fields["left"] = (int)rect.Left;
+            Fields["right"] = (int)rect.Right;
+            Fields["top"] = (int)rect.Top;
+            Fields["bottom"] = (int)rect.Bottom;
+        }
+
+    }
+
+    // ========= Error handling =============
+
+    public enum ErrorCodes : int
+    {
+        OK,
+        PARSE_ERROR,
+        UNSUPPORTED_ACTION,
+        MISSING_PARAM,
+        RUNTIME_ERROR,
+        NOT_FOUND,
+        UNSUPPORTED_TYPE,
+        INVALID_VALUE,
+    }
+
+    class ErrorReply : Reply
+    {
+        public ErrorReply(ErrorCodes statusCode, string message)
+            : base(statusCode)
+        {
+            Fields["message"] = message;
+        }
+    }
+    class ErrorReplyException : Exception
+    {
+        public ErrorReply Reply;
+
+        public ErrorReplyException(ErrorCodes statusCode, string message)
+        {
+            Reply = new ErrorReply(statusCode, message);
+        }
+    }
+}
