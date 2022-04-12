ea695ab3fbba82dce882106d10a56bf99d2e5479
eltio <i.n.1998@inbox.ru>
2022-04-12 23:05:29 +0300

    improve injected module structure

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import json
 import six
+from pywinauto.backend import Singleton
+from pywinauto.actionlogger import ActionLogger
 
-from . import injector
-from ...backend import Singleton
+from .injector import Injector
+from .channel import Pipe
 
 # backend exit codes enum
 OK=0
@@ -33,8 +35,20 @@ class ConnectionManager(object):
 
     def _get_pipe(self, pid):
         if pid not in self._pipes:
-            self._pipes[pid] = injector.create_pipe(pid)
+            self._pipes[pid] = self._create_pipe(pid)
         return self._pipes[pid]
+
+    def _create_pipe(self, pid):
+        pipe_name = 'pywinauto_{}'.format(pid)
+        pipe = Pipe(pipe_name)
+        if pipe.connect(n_attempts=1):
+            ActionLogger().log('Pipe {} found, looks like dll has been injected already'.format(pipe_name))
+            return pipe
+        else:
+            ActionLogger().log('Pipe {} not found, injecting dll to the process'.format(pipe_name))
+            injected_bootstrap = Injector(pid, 'dotnet', 'bootstrap')
+            pipe.connect()
+            return pipe
     
     def call_action(self, action_name, pid, **params):
         command = json.dumps({'action': action_name, **params})
--- channel.py
+++ channel.py
@@ -4,6 +4,7 @@ import win32file
 import win32pipe
 import winerror
 import sys
+from pywinauto.actionlogger import ActionLogger
 
 
 class BrokenPipeError(Exception):
@@ -14,7 +15,7 @@ class Pipe:
         self.name = name
         self.handle = None
 
-    def connect(self, n_attempts=100, delay=1):
+    def connect(self, n_attempts=30, delay=1):
         for i in range(n_attempts):
             try:
                 self.handle = win32file.CreateFile(
@@ -32,14 +33,12 @@ class Pipe:
                         None,
                         None,
                     )
-                # if ret != winerror.S_OK:
-                #     print('SetNamedPipeHandleState exit code is {}'.format(ret))
-
-                #print(f'Connected to the pipe {self.name}, {self.handle=}')
+                ActionLogger().log('Connected to the pipe {}'.format(self.name))
                 break
             except pywintypes.error as e:
                 if e.args[0] == winerror.ERROR_FILE_NOT_FOUND:
-                    #print("Attempt {}: connection failed, trying again".format(i + 1))
+                    ActionLogger().log('Attempt {}/{}: failed to connect to the pipe {}'.format(i+1, n_attempts,
+                                                                                                self.name))
                     time.sleep(delay)
                 else:
                     raise BrokenPipeError('Unexpected pipe error: {}'.format(e))
@@ -52,7 +51,6 @@ class Pipe:
             win32file.WriteFile(self.handle, string.encode('utf-8'))
             win32file.FlushFileBuffers(self.handle)
             resp = win32file.ReadFile(self.handle, 64 * 1024)
-            #print('message: {}'.format(resp[1].decode(sys.getdefaultencoding())))
             return resp[1].decode(sys.getdefaultencoding())
         except pywintypes.error as e:
             if e.args[0] == winerror.ERROR_BROKEN_PIPE:
