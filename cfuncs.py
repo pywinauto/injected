18c3f183425885adcc711cbae9cad82a8c72ebea
eltio <i.n.1998@inbox.ru>
2022-04-23 19:30:02 +0300

    Set channel encoding to UTF-8, changes in cfuncs.py

--- cfuncs.py
+++ cfuncs.py
@@ -2,65 +2,74 @@
 injector work needs full redefinition with ctypes compatible
 """
 
-import ctypes
 import win32con
-from ctypes.wintypes import BOOL
-from ctypes.wintypes import DWORD
-from ctypes.wintypes import HANDLE
-from ctypes.wintypes import LPVOID
-from ctypes.wintypes import LPCVOID
-from pywinauto.windows.win32structures import SECURITY_ATTRIBUTES
+from ctypes import Structure, sizeof, alignment
+from ctypes import c_char_p, c_wchar_p
+from ctypes import POINTER
+from ctypes import windll
+from ctypes.wintypes import BOOL, DWORD, HANDLE, LPVOID, LPCVOID
+
+# C:/PROGRA~1/MICROS~4/VC98/Include/winbase.h 223
+class SECURITY_ATTRIBUTES(Structure):
+    _fields_ = [
+        # C:/PROGRA~1/MICROS~4/VC98/Include/winbase.h 223
+        ('nLength', DWORD),
+        ('lpSecurityDescriptor', LPVOID),
+        ('bInheritHandle', BOOL),
+    ]
+assert sizeof(SECURITY_ATTRIBUTES) == 12 or sizeof(SECURITY_ATTRIBUTES) == 24, sizeof(SECURITY_ATTRIBUTES)
+assert alignment(SECURITY_ATTRIBUTES) == 4 or alignment(SECURITY_ATTRIBUTES) == 8, alignment(SECURITY_ATTRIBUTES)
 
 PAGE_READWRITE = win32con.PAGE_READWRITE
 WAIT_TIMEOUT = win32con.WAIT_TIMEOUT
 PROCESS_ALL_ACCESS = win32con.PROCESS_ALL_ACCESS
 VIRTUAL_MEM = ( win32con.MEM_RESERVE | win32con.MEM_COMMIT )
-LPCSTR = LPCTSTR = ctypes.c_char_p
-LPWTSTR = ctypes.c_wchar_p
-LPDWORD = PDWORD = ctypes.POINTER(DWORD)
+LPCSTR = LPCTSTR = c_char_p
+LPWTSTR = c_wchar_p
+LPDWORD = PDWORD = POINTER(DWORD)
 LPTHREAD_START_ROUTINE = LPVOID
-LPSECURITY_ATTRIBUTES = ctypes.POINTER(SECURITY_ATTRIBUTES)
+LPSECURITY_ATTRIBUTES = POINTER(SECURITY_ATTRIBUTES)
 
-OpenProcess = ctypes.windll.kernel32.OpenProcess
+OpenProcess = windll.kernel32.OpenProcess
 OpenProcess.restype = HANDLE
 OpenProcess.argtypes = (DWORD, BOOL, DWORD)
 
-VirtualAllocEx = ctypes.windll.kernel32.VirtualAllocEx
+VirtualAllocEx = windll.kernel32.VirtualAllocEx
 VirtualAllocEx.restype = LPVOID
 VirtualAllocEx.argtypes = (HANDLE, LPVOID, DWORD, DWORD, DWORD)
 
-ReadProcessMemory = ctypes.windll.kernel32.ReadProcessMemory
+ReadProcessMemory = windll.kernel32.ReadProcessMemory
 ReadProcessMemory.restype = BOOL
 ReadProcessMemory.argtypes = (HANDLE, LPCVOID, LPVOID, DWORD, DWORD)
 
-WriteProcessMemory = ctypes.windll.kernel32.WriteProcessMemory
+WriteProcessMemory = windll.kernel32.WriteProcessMemory
 WriteProcessMemory.restype = BOOL
 WriteProcessMemory.argtypes = (HANDLE, LPVOID, LPCVOID, DWORD, DWORD)
 
-CreateRemoteThread = ctypes.windll.kernel32.CreateRemoteThread
+CreateRemoteThread = windll.kernel32.CreateRemoteThread
 CreateRemoteThread.restype = HANDLE
 CreateRemoteThread.argtypes = (HANDLE, LPSECURITY_ATTRIBUTES, DWORD, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPDWORD)
 
-GetModuleHandleA = ctypes.windll.kernel32.GetModuleHandleA
+GetModuleHandleA = windll.kernel32.GetModuleHandleA
 GetModuleHandleA.restype = HANDLE
 GetModuleHandleA.argtypes = (LPCTSTR,)
 
-LoadLibraryA = ctypes.windll.kernel32.LoadLibraryA
+LoadLibraryA = windll.kernel32.LoadLibraryA
 LoadLibraryA.restype = HANDLE
 LoadLibraryA.argtypes = (LPCTSTR,)
 
-GetModuleHandleW = ctypes.windll.kernel32.GetModuleHandleW
+GetModuleHandleW = windll.kernel32.GetModuleHandleW
 GetModuleHandleW.restype = HANDLE
 GetModuleHandleW.argtypes = (LPWTSTR,)
 
-LoadLibraryW = ctypes.windll.kernel32.LoadLibraryW
+LoadLibraryW = windll.kernel32.LoadLibraryW
 LoadLibraryW.restype = HANDLE
 LoadLibraryW.argtypes = (LPWTSTR,)
 
-GetProcAddress = ctypes.windll.kernel32.GetProcAddress
+GetProcAddress = windll.kernel32.GetProcAddress
 GetProcAddress.restype = LPVOID
 GetProcAddress.argtypes = (HANDLE, LPCTSTR)
 
-WaitForSingleObject = ctypes.windll.kernel32.WaitForSingleObject
+WaitForSingleObject = windll.kernel32.WaitForSingleObject
 WaitForSingleObject.restype = DWORD
 WaitForSingleObject.argtypes = (HANDLE, DWORD)
